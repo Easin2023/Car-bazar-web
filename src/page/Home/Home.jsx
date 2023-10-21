137891a80adf0b55cfd3288dfb19d1369f966449
Easin2023 <mdeasinarafat016456@gmail.com>
2023-10-21 20:07:43 +0600

    new extra section added

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../component/Banner/Banner";
 import CarItemList from "../../component/CarItemList/CarItemList";
+import ExtraSection from "../../component/ExtraSection/ExtraSection";
 
 const Home = () => {
      return (
           <div>
                <Banner></Banner>
                <CarItemList></CarItemList>
+               <ExtraSection></ExtraSection>
           </div>
      );
 };
